19fadf9a850d07da5c152787700c9a48a949e3d9
Fulya Sariyar <fulyasariyar@outlook.com>
2022-08-31 12:57:50 +0300

    two

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,19 @@
 import {gql} from 'apollo-server'
-//Schema
-const typeDefs = gql 
-' type Query # Notes for homepage {notes:[Note!]!} type AuthorX {id:ID! name:String! age:Int! photo: String} type Note{id:ID! title:String! description: String! aouthor:Author!}'
 
 
+const typeDefs = gql`
+union SearchResult =Book | Author;  
+type Book{id:Id!
+     title:String! 
+     description: String!
+      numberOfSold: Int
+       price:Float!
+        isNew:Boolean
+    }
+type Author {
+            id: ID!
+            name: String!
+            age:Int!
+        }
+        ` 
 module.export = typeDefs;
