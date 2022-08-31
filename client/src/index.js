646cfa95aacf26ca20211518bb828fb693ed866c
Fulya Sariyar <fulyasariyar@outlook.com>
2022-08-31 15:23:15 +0300

    Created Apollo Client

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import {ApolloProvider ,ApolloClient, InMemoryCache} from '@apollo/client'
+
+//Apollo Client
+const client = new ApolloClient({
+  uri:"http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- server/src/schema.js
+++ server/src/schema.js
@@ -3,7 +3,7 @@ const {gql} =require('apollo-server')
 
 const typeDefs = gql`
 type Query{
-    # Notes for homepage
+    "Notes for homepage"
     notes:[Note!]!
 }
    "Author Description"
