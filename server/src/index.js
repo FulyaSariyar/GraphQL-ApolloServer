9d2c103a018497afc09201a0acff42a6d54ac420
Fulya Sariyar <fulyasariyar@outlook.com>
2022-08-31 13:36:28 +0300

    Running ok.

--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,10 @@
-import {gql} from 'apollo-server'
+const {ApolloServer} = require('apollo-server');
+const typeDefs = require ('./schema')
 
+const server = new ApolloServer({
+    typeDefs : typeDefs
+})
 
-const typeDefs = gql`
-type Query{
-    # Notes for homepage
-    notes:[Note!]!
-}
-   "Author Description"
-   type Author {
-    id:ID!
-    "The name of the author"
-    name:String!
-    "The age of the author"
-    age:Int!
-    "The photo URL of the author"
-    photo:String
-   }
-
-   "Notes Description"
-   type Note{
-    id:ID!
-      "The title of the note"
-    title:String!
-    "The description of the note"
-    description:String!
-   }
-        ` 
-module.export = typeDefs;
+server.listen().then(() => {
+    console.log('Server is running on port 4000, Query at: https://studio.apollographql.com/dev')
+});
---
+++ server/src/schema.js
@@ -0,0 +1,30 @@
+const {gql} =require('apollo-server')
+
+
+const typeDefs = gql`
+type Query{
+    # Notes for homepage
+    notes:[Note!]!
+}
+   "Author Description"
+   type Author {
+    id:ID!
+    "The name of the author"
+    name:String!
+    "The age of the author"
+    age:Int!
+    "The photo URL of the author"
+    photo:String
+   }
+
+   "Notes Description"
+   type Note{
+    id:ID!
+      "The title of the note"
+    title:String!
+    "The description of the note"
+    description:String!
+   }
+        ` 
+
+module.exports = typeDefs;
